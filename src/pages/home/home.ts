04987f27248da531dd29698fa175433f3ac6acc2
Ikazuchi <namph@nal.vn>
2017-07-21 01:26:06 +0700

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { StreamingMedia, StreamingVideoOptions, StreamingAudioOptions } from '@ionic-native/streaming-media';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private streamingMedia: StreamingMedia) { }
 
+  startVideo() {
+    let options: StreamingVideoOptions = {
+      successCallback: () => { console.log('Finished Video') },
+      errorCallback: (e) => { console.log('Error: ', e) },
+      orientation: 'portrait'
+    };
+
+    // http://www.sample-videos.com/
+    this.streamingMedia.playVideo('http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_30mb.mp4', options);
+  }
+
+  startAudio() {
+    let options: StreamingAudioOptions = {
+      successCallback: () => { console.log('Finished Audio') },
+      errorCallback: (e) => { console.log('Error: ', e) },
+      initFullscreen: false // iOS only!
+    };
+
+    //http://soundbible.com/2196-Baby-Music-Box.html
+    this.streamingMedia.playAudio('http://soundbible.com/grab.php?id=2196&type=mp3', options);
+  }
+
+  stopAudio() {
+    this.streamingMedia.stopAudio();
   }
 
 }
